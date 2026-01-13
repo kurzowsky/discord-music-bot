aef6402c102249513cecd62ca9758fb0fdd8f39e
kurzowsky <147747831+kurzowsky@users.noreply.github.com>
2026-01-13 16:28:16 +0100

    update user-agent

--- config.py
+++ config.py
@@ -56,6 +56,8 @@ YDL_OPTIONS = {
     'default_search': 'auto',
     'source_address': '0.0.0.0',
     'cookiefile': 'cookies.txt',
+    'force_ipv4': True,
+    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
 }
 
 # Opcje przetwarzania dźwięku (FFmpeg)
